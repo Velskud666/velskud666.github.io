d594a96fe1179fc035e3606d29594003ecd4d057
Velskud666 <130354427+Velskud666@users.noreply.github.com>
2024-10-08 07:35:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<input type="number" id="yourStunRes" placeholder="Your Stun RES">
+<input type="number" id="yourDebilitationRes" placeholder="Your Debilitation RES">
+<input type="number" id="yourSilenceRes" placeholder="Your Silence RES">
+
+<input type="number" id="enemyStunSuccess" placeholder="Enemy Stun Success">
+<input type="number" id="enemyDebilitationSuccess" placeholder="Enemy Debilitation Success">
+<input type="number" id="enemySilenceSuccess" placeholder="Enemy Silence Success">
